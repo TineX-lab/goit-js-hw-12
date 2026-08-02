8cfd9f8a6b6a9850fd62de55f6b84f41c675db59
Oleksandr <alexsandrolegov@gmail.com>
2026-08-02 13:14:43 +0200

    feat: add pagination, smooth scroll and refactor API with async/await

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API_KEY = '56853055-640f52739347c513669db01a2';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export const getImagesByQuery = async (query, page = 1) => {
+  const response = await axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: page,
+      per_page: 15,
+    },
+  });
+  
+  return response.data;
+};
---
+++ src/js/render-functions.js
@@ -0,0 +1,53 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+const loadMoreBtn = document.querySelector('.load-more');
+
+// Ініціалізація SimpleLightbox
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export const createGallery = (images) => {
+  const markup = images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
+    <li class="gallery-item">
+      <a class="gallery-link" href="${largeImageURL}">
+        <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
+        <div class="info">
+          <p class="info-item"><b>Likes</b> ${likes}</p>
+          <p class="info-item"><b>Views</b> ${views}</p>
+          <p class="info-item"><b>Comments</b> ${comments}</p>
+          <p class="info-item"><b>Downloads</b> ${downloads}</p>
+        </div>
+      </a>
+    </li>
+  `).join('');
+
+  galleryContainer.insertAdjacentHTML('beforeend', markup);
+  
+  // Оновлюємо лайтбокс після додавання нових елементів
+  lightbox.refresh();
+};
+
+export const clearGallery = () => {
+  galleryContainer.innerHTML = '';
+};
+
+export const showLoader = () => {
+  loader.classList.remove('is-hidden');
+};
+
+export const hideLoader = () => {
+  loader.classList.add('is-hidden');
+};
+
+export const showLoadMoreButton = () => {
+  loadMoreBtn.classList.remove('is-hidden');
+};
+
+export const hideLoadMoreButton = () => {
+  loadMoreBtn.classList.add('is-hidden');
+};
